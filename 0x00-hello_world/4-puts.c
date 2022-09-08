75ee579e17937c2fe83cddab184b8fcdb1248a23
MsVee55 <precious.a.vivian@gmail.com>
2022-09-08 23:15:29 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>                                           
+/**                                                          
+ * main - using the put function to print string             
+ *                                                           
+ * Description: using the main function to print             
+ *  Programming is like building a multilingual puzzle       
+ *  Return: 0                                                
+ */                                                          
+int main(void)                                               
+{                                                            
+        puts("\"Programming is like building a multilingual p
+uzzle");                                                     
+                return (0);                                  
+}
