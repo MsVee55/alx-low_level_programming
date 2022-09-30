d297a0b488d253d884a90527629b33215484db07
“MsVee55git config --global user.name “MsVee55 <precious.a.vivian@gmail.comgit config --global user.email precious.a.vivian@gmail.com>
2022-09-30 16:39:56 -0700

    task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: EJ GIBSON
+ * Auth: Precious Augustine
  * Desc: Header file containing the prototypes of all functions
  *       included in the static library libholberton.a.
  */
