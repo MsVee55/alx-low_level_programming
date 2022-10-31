fa1c626b5ee5394b5b9ce0742edd0027aec1bb08
MsVee55 <precious.v.augustine@gmail.com>
2022-10-31 10:58:26 -0700

    Read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,46 @@
-#include <stdio.h>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
- * @filename: file.
- * @letters: Number of letters it should read and print.
- * Return: Actual number of letters it could read and print.
+ * read_textfile- function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: pointer to file to be read
+ * @letters: number of letters to be read
+ * Return: actual number of letters read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, res_read, res_write;
+	int fd;
+	ssize_t sz, buf2;
 	char *buf;
 
 	if (filename == NULL)
+	{
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	}
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
-	res_read = read(fd, buf, letters);
-	if (res_read == -1)
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	res_write = write(STDOUT_FILENO, buf, res_read);
-	if (res_write == -1 || res_read != res_write)
+	sz = read(fd, buf, letters);
+	if (sz == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
-	free(buf);
+	buf2 = write(STDOUT_FILENO, buf, sz);
+	if (buf2 == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 	close(fd);
-	return (res_write);
+	free(buf);
+	return (buf2);
 }
